983df76b97bfcfaaf879110426d046d887cf29fb
Severn Lortie <severn@distributive.network>
2024-07-23 16:00:29 -0400

    improvement(job): refactor BiFrost2 APIs out of the Job class (serialization, env)

--- dcp/api/__init__.py
+++ dcp/api/__init__.py
@@ -1,10 +1,14 @@
 from .compute_for import compute_for_maker
 from .job import job_maker
 from .result_handle import result_handle_maker 
+from .job_serializers import Serializers
+from .job_env import Env
 
 sub_classes = {
     'Job': job_maker,
     'ResultHandle': result_handle_maker,
+    'Serializers': Serializers,
+    'Env': Env
 }
 
 __all__ = ['compute_for_maker', 'sub_classes']
--- dcp/api/job.py
+++ dcp/api/job.py
@@ -1,6 +1,9 @@
 import pythonmonkey as pm
 import pickle
 from ..js import utils
+from .. import dry
+from .job_serializers import Serializers
+from .job_env import Env
 from collections.abc import Iterator
 from types import FunctionType
 import urllib.parse
@@ -9,83 +12,60 @@ import urllib.parse
 def job_maker(super_class):
     class Job(super_class):
         def __init__(self, *args, **kwargs):
-            super.__init__(*args, **kwargs)
-            self.work_function = args[0]
+            compute_for_js = pm.eval("globalThis.dcp.compute.for")
+            job_js = dry.aio.blockify(compute_for_js)(*args, **kwargs)
+            super().__init__(job_js)
+
+            work_function_candidates = [arg for arg in args if isinstance(arg, FunctionType)]
+            if len(work_function_candidates) > 1:
+                raise Exception("More than one work function candidate detected. This is a bug")
+
+            self._wrapper_set_attribute("_work_function", work_function_candidates[0])
+            self._wrapper_set_attribute("_serializers_instance", Serializers())
+            self._wrapper_set_attribute("_env_instance", Env())
+
+        @property
+        def serializers(self):
+            return self._wrapper_get_attribute("_serializers_instance").serializers
+
+        @property
+        def env(self):
+            return self._wrapper_get_attribute("_env_instance").env
 
         def exec(self, *args, **kwargs):
-            def validateSerializers():
-                required_keys = ['name', 'interrogator', 'serializer', 'deserializer']
-                for i in range(len(self.serializers)):
-                    serializer = self.serializers[i]
-                    missing_keys = [key for key in required_keys if key not in serializer]
-                    if len(missing_keys) > 0:
-                        raise TypeError(f"Serializer at index {i} is missing keys: {missing_keys}")
-                    if len(serializer.name) > 256:
-                        raise TypeError(f"Serializer at index {i} has name '{serializer.name}' which exceeds 256 characters")
-            validateSerializers()
-
-            class IteratorWrapper:
-                def __init__(self, iterator):
-                    self.iterator = iterator
-
-                def __iter__(self):
-                    return self
-
-                def __next__(self):
-                    value = next(self.iterator)
-                    return serialize(value)
-
-            def serialize(value):
-                primitive_types = (int, float, bool, str, bytes)
-                if isinstance(value, primitive_types):
-                    return value
-                super_range_object = pm.eval("globalThis.dcp['range-object'].SuperRangeObject")
-                if utils.instanceof(value, super_range_object):
-                    return value
-                if isinstance(value, Iterator):
-                    return IteratorWrapper(value)
-
-                for serializer in self.serializers:
-                    if serializer.interrogate(value):
-                        serialized_value = serializer.serialize(value)
-                        serialized_serializer_name = serializer.name.encode()
-                        return serialized_serializer_name + serialized_value
+            self._wrapper_get_attribute("_serializers_instance").validate_serializers()
 
             serialized_input_data = []
             serialized_arguments = []
             serialized_work_function = b''
 
             for input_slice in self.js_ref.jobInputData:
-                serialized_input_data.append(serialize(input_slice))
+                serialized_slice = self._wrapper_get_attribute("_serializers_instance").serialize(input_slice)
+                serialized_input_data.append(serialized_slice)
             for argument in self.js_ref.jobArguments:
-                serialized_arguments.append(serialize(argument))
+                serialized_argument = self._wrapper_get_attribute("_serializers_instance").serialize(argument)
+                serialized_arguments.append(serialized_argument)
 
             if self.js_ref.worktime == 'pyodide':
+                # TODO: this is where the Bf2 workfn should be inserted
                 self.js_ref.workFunctionURI = urllib.parse.quote("");
-            else:
-                if isinstance(self.work_function, str):
-                    self.js_ref.workFunctionURI = urllib.parse.quote(self.work_function)
 
-            def convert_job_env_to_arguments():
-                args = []
-                for env_key in self.env:
-                    args.append(f"{env_key}={self.env[env_key]}")
-                return args;
-            env_args = convert_job_env_to_arguments();
+            env_args = self._wrapper_get_attribute("_env_instance").convert_to_arguments();
 
             #TODO serialize JobFS and emplace in argument array. For now this is just a placeholder
             offset_to_argument_vector = 4 + len(env_args)
-            self.js_ref.jobArguments  = offset_to_argument_vector + ["gzImage", b''] + ["env"] + env_args + serialized_arguments + serialized_work_function
+            self.js_ref.jobInputData = serialized_input_data
+            self.js_ref.jobArguments = [offset_to_argument_vector] + ["gzImage", b''] + ["env"] + env_args + serialized_arguments + [serialized_work_function]
 
-            def on(self, *args):
-                if len(args) > 1 and callable(args[1]):
-                    event_name = args[0]
-                    event_cb = args[1]
-                    self.js_ref.on(event_name, event_cb)
-                else:
-                    event_name = args[0]
-                    def decorator(fn):
-                        self.js_ref.on(event_name, fn)
-                    return decorator
+        def on(self, *args):
+            if len(args) > 1 and callable(args[1]):
+                event_name = args[0]
+                event_cb = args[1]
+                self.js_ref.on(event_name, event_cb)
+            else:
+                event_name = args[0]
+                def decorator(fn):
+                    self.js_ref.on(event_name, fn)
+                return decorator
     return Job
 
