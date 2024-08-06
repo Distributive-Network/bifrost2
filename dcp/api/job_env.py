061578a7b7b556d406a521381ba74fc029185d44
Severn Lortie <severn@distributive.network>
2024-08-06 16:00:40 -0400

    bugfix(job): return empty array when no env vars

--- dcp/api/job_env.py
+++ dcp/api/job_env.py
@@ -1,8 +1,17 @@
+"""
+Stores and converts Job Env arguments for Pyodide worktime.
+
+Author: Severn Lortie <severn@distributive.network>
+Date: Aug 2024
+"""
+
 class Env:
     def __init__(self):
         self.env = {}
     def convert_to_arguments(self):
-        args = []
+        if len(self.env) < 1:
+            return []
+        args = ["env"]
         for env_key in self.env:
             args.append(f"{env_key}={self.env[env_key]}")
         return args
