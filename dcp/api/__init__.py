dbf82304f0ba7a81293351a55cf2e9fb1aea8b14
Severn Lortie <severn@distributive.network>
2024-08-06 16:01:51 -0400

    improvement(init.py): add Env and Modules as exports

--- dcp/api/__init__.py
+++ dcp/api/__init__.py
@@ -3,12 +3,14 @@ from .job import job_maker
 from .result_handle import result_handle_maker 
 from .job_serializers import Serializers
 from .job_env import Env
+from .job_modules import Modules
 
 sub_classes = {
     'Job': job_maker,
     'ResultHandle': result_handle_maker,
     'Serializers': Serializers,
-    'Env': Env
+    'Env': Env,
+    'Modules': Modules
 }
 
 __all__ = ['compute_for_maker', 'sub_classes']
