98a69b13653a8956720d7d32e4e821fef6945e6a
Severn Lortie <severn@distributive.network>
2024-08-06 16:02:39 -0400

    improvement(serialization): deserialize job results

--- dcp/api/job_serializers.py
+++ dcp/api/job_serializers.py
@@ -5,30 +5,40 @@ Author: Severn Lortie <severn@distributive.network>
 Date: July 2024
 """
 
-import numpy as np
-import pickle
-from io import BytesIO
+
+import cloudpickle
+import dill
+import pythonmonkey as pm
+from ..js import utils
+from collections.abc import Iterator
 
 class Serializers:
     def __init__(self):
         def numpy_save_interrogate(value):
+            import numpy as np
             return isinstance(value, np.ndarray)
         def numpy_save_serialize(value):
+            import numpy as np
+            from io import BytesIO
             byte_buffer = BytesIO()
             np.save(byte_buffer, value)
             buffer.seek(0)
             return buffer.read()
         def numpy_save_deserialize(value):
+            import numpy as np
+            from io import BytesIO
             byte_buffer = BytesIO()
             buffer.seek(0)
-            return numpy.load(value)
+            return np.load(value)
         
         def pickle_interrogate(value):
             return True
         def pickle_serialize(value):
-            return pickle.dumps(value)
+            import cloudpickle
+            return cloudpickle.dumps(value)
         def pickle_deserialize(value):
-            return pickle.loads(value)
+            import cloudpickle
+            return cloudpickle.loads(value)
 
         self.serializers = [
             {
@@ -55,6 +65,7 @@ class Serializers:
             if len(serializer["name"]) > 256:
                 raise TypeError(f"Serializer at index {i} has name '{serializer.name}' which exceeds 256 characters")
 
+    # TODO: This code is duplicated across the job work function. See if possible to use this in both places
     def serialize(self, value):
         class IteratorWrapper:
             def __init__(self, iterator):
@@ -78,13 +89,48 @@ class Serializers:
 
         for serializer in self.serializers:
             if serializer["interrogator"](value):
-                serialized_value_bytes = serializer["serialize"](value)
+                serialized_value_bytes = serializer["serializer"](value)
                 serialized_serializer_name_bytes = serializer["name"].encode('utf-8')
+                serializer_name_length = len(serializer["name"])
+                serializer_name_length_byte = bytearray(serializer_name_length.to_bytes(1, byteorder='big'))
                 serialized_serializer_name_byte_array = bytearray(serialized_serializer_name_bytes)
                 serialized_value_byte_array = bytearray(serialized_value_bytes)
-                return serialized_serializer_name_byte_array + serialized_value_byte_array
+                return serializer_name_length_byte + serialized_serializer_name_byte_array + serialized_value_byte_array
 
+    # TODO: This code is duplicated across the job work function. See if possible to use this in both places
+    def deserialize(self, value):
+        if isinstance(value, memoryview):
+            value = bytearray(value.tobytes())
+        elif not isinstance(value, bytearray):
+            return value
+        serializer_name_length = value[0]
+        if serializer_name_length > len(value):
+            return value
+        name_start_idx = 1
+        name_end_idx = serializer_name_length + 1
+        serializer_name_bytes = value[name_start_idx:name_end_idx]
+        del value[0:name_end_idx]
+        serializer_name = serializer_name_bytes.decode('utf-8')
+        allowed_serializer_names = [serializer["name"] for serializer in self.serializers]
+        if serializer_name not in allowed_serializer_names:
+            return value
 
+        serializer = next((serializer for serializer in self.serializers if serializer["name"] == serializer_name), None)
+        return serializer["deserializer"](value)
+
+    def convert_to_arguments(self):
+        serialized_serializers = []
+        for serializer in self.serializers:
+            serialized_serializers.append({
+                "name": serializer["name"],
+                "interrogator": dill.source.getsource(serializer["interrogator"], lstrip=True),
+                "serializer":   dill.source.getsource(serializer["serializer"], lstrip=True),
+                "deserializer": dill.source.getsource(serializer["deserializer"], lstrip=True)
+            })
+        return bytearray(cloudpickle.dumps(serialized_serializers))
+
+    def empty(self):
+        return len(self.serializers) < 1
 
 
 
---
+++ dcp/api/pyodide_work_function.py
@@ -0,0 +1,110 @@
+"""
+The BiFrost2 work function. Handles deserializing/serializing job data and running
+the real (user provided) work function.
+
+Author: Severn Lortie <severn@distributive.network>
+Date: Aug 2024
+"""
+
+work_function_string = """
+import numpy
+import cloudpickle
+import sys
+import js
+from collections.abc import Iterator
+
+def console_log(data):
+    js.globalThis.postMessage(data)
+
+def eval_function(function_string):
+    def user_code_namespace():
+        locals = {}
+        exec(function_string, {"dcp": dcp}, locals)
+        return locals
+    locals = user_code_namespace()
+    function_name = next(iter(locals))
+    return locals[function_name]
+
+def bifrost2_setup():
+    meta_arguments = sys.argv.pop()
+    work_function_string = meta_arguments[0]
+
+    user_work_function = eval_function(work_function_string)
+    if len(meta_arguments) < 2:
+        user_work_function()
+        return
+
+    serialized_serializers = meta_arguments[1]
+    serializers = cloudpickle.loads(serialized_serializers)
+
+    for serializer in serializers:
+        serializer["interrogator"] = eval_function(serializer["interrogator"])
+        serializer["serializer"]   = eval_function(serializer["serializer"])
+        serializer["deserializer"] = eval_function(serializer["deserializer"])
+
+    def serialize(value):
+        class IteratorWrapper:
+            def __init__(self, iterator):
+                self.iterator = iterator
+
+            def __iter__(self):
+                return self
+
+            def __next__(self):
+                value = next(self.iterator)
+                return serialize(value)
+
+        primitive_types = (int, float, bool, str, bytes)
+        if isinstance(value, primitive_types):
+            return value
+        if isinstance(value, Iterator):
+            return IteratorWrapper(value)
+
+        for serializer in serializers:
+            if serializer["interrogator"](value):
+                serialized_value_bytes = serializer["serializer"](value)
+                serialized_serializer_name_bytes = serializer["name"].encode('utf-8')
+                serializer_name_length = len(serializer["name"])
+                serializer_name_length_byte = bytearray(serializer_name_length.to_bytes(1, byteorder='big'))
+                serialized_serializer_name_byte_array = bytearray(serialized_serializer_name_bytes)
+                serialized_value_byte_array = bytearray(serialized_value_bytes)
+                return serializer_name_length_byte + serialized_serializer_name_byte_array + serialized_value_byte_array
+
+    def deserialize(value):
+        if isinstance(value, memoryview):
+            value = bytearray(value.tobytes())
+        elif not isinstance(value, bytearray):
+            return value
+        serializer_name_length = value[0]
+        if serializer_name_length > len(value):
+            return value
+        name_start_idx = 1
+        name_end_idx = serializer_name_length + 1
+        serializer_name_bytes = value[name_start_idx:name_end_idx]
+        del value[0:name_end_idx]
+        serializer_name = serializer_name_bytes.decode('utf-8')
+        allowed_serializer_names = [serializer["name"] for serializer in serializers]
+        if serializer_name not in allowed_serializer_names:
+            return value
+
+        serializer = next((serializer for serializer in serializers if serializer["name"] == serializer_name), None)
+        return serializer["deserializer"](value)
+
+    for i in range(len(sys.argv)):
+        arg = deserialize(sys.argv[i])
+        if arg is None:
+            continue
+        sys.argv[i] = arg
+
+    original_set_slice_handler = dcp.set_slice_handler
+    def set_slice_handler(slice_handler):
+        def slice_handler_deserialization_wrapper(serialized_datum):
+            datum = deserialize(serialized_datum)
+            result = slice_handler(datum)
+            serialized_result = serialize(result)
+            return serialized_result
+        original_set_slice_handler(slice_handler_deserialization_wrapper)
+    dcp.set_slice_handler = set_slice_handler
+    user_work_function()
+bifrost2_setup()
+"""
