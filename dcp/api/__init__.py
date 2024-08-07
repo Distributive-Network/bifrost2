5314ca68d4228d2c5528a51655be135e30bcab30
Severn Lortie <severn@distributive.network>
2024-08-07 16:48:15 -0400

    improvement(serialization): dedup serialization code across job and and pyodide_work_function

--- dcp/api/__init__.py
+++ dcp/api/__init__.py
@@ -1,18 +1,13 @@
 from .compute_for import compute_for_maker
+from .compute_do import compute_do_maker
 from .job import job_maker
 from .result_handle import result_handle_maker 
-from .job_serializers import Serializers
-from .job_env import Env
-from .job_modules import Modules
 from .job_fs import JobFS
 
 sub_classes = {
     'Job': job_maker,
     'ResultHandle': result_handle_maker,
-    'Serializers': Serializers,
-    'Env': Env,
-    'Modules': Modules
 }
 
-__all__ = ['compute_for_maker', 'sub_classes', 'JobFS']
+__all__ = ['compute_for_maker', 'compute_do_maker' 'sub_classes', 'JobFS']
 
--- dcp/api/job_modules.py
+++ dcp/api/job_modules.py
@@ -1,16 +1,13 @@
 """
-Stores and converts Job modules (Pyodide core packages) for Pyodide worktime.
+Converts Job modules (Pyodide core packages) for Pyodide worktime.
 
 Author: Severn Lortie <severn@distributive.network>
 Date: Aug 2024
 """
 
-class Modules:
-    def __init__(self):
-        self.modules = []
-    def convert_to_requires(self):
-        requires = []
-        for module in self.modules:
-            requires.append(f"pyodide-{module}/pyodide-{module}.js")
-        return requires
+def convert_modules_to_requires(modules):
+    requires = []
+    for module in modules:
+        requires.append(f"pyodide-{module}/pyodide-{module}.js")
+    return requires
 
